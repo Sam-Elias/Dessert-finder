33b4ff2fde508c8a2a40240e0e24d405d0a3a854
Samir Elias <samirelias@gmail.com>
2018-10-29 15:14:40 -0700

    created and styled AppHeader component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppHeader from './components/AppHeader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="header">
-          Home-made dessert finder
-        </header>
+        <AppHeader />
         <nav className="sidebar">
+          <div>
           <div>Search</div>
           <ul className="sidebar">
             <li>dessert 1</li>
             <li>dessert 2</li>
             <li>dessert 3</li>
           </ul>
+          </div>
         </nav>
         <div className="map">
-          MAP
+          <div>MAP</div>
         </div>
         <footer className="footer">
-          Made by Sam Elias
+          <div>Made by Sam Elias</div>
         </footer>
 
       </div>
---
+++ src/components/AppHeader.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class AppHeader extends Component {
+
+  render() {
+    return (
+      <header className="header">
+          Home-made dessert finder
+      </header>
+    )
+  }
+}
+
+export default AppHeader
