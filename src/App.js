338b050c0301b15d3e9c8b6a9e6d1ff737bd3e84
Samir Elias <samirelias@gmail.com>
2018-10-29 16:22:25 -0700

    created and styled AppSidebar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
+import AppSidebar from './components/AppSidebar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <AppHeader />
-        <nav className="sidebar">
-          <div>
-          <div>Search</div>
-          <ul className="sidebar">
-            <li>dessert 1</li>
-            <li>dessert 2</li>
-            <li>dessert 3</li>
-          </ul>
-          </div>
-        </nav>
+        <AppSidebar />
         <div className="map">
           <div>MAP</div>
         </div>
---
+++ src/components/AppSidebar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class AppSidebar extends Component {
+
+  render() {
+    return (
+      <nav className="sidebar">
+        <input type="search" id="search" placeholder="Enter a dessert type"></input>
+        <ul className="sidebar">
+          
+        </ul>
+    </nav>
+    )
+  }
+}
+
+export default AppSidebar
